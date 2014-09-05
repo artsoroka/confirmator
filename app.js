b30a3fc8a71069c300fd6d8afc93552ddce707c6
artsoroka <githubrepository@yandex.ru>
2014-09-05 21:07:57 +0000

    added express.js as a server

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var http = require('http'),
-    fs = require('fs'); 
- 
-var server = http.createServer(function (req, res) {
-  html = fs.readFileSync('index.html'); 
+var express = require('express');
+var app = express();
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(html);
-}).listen(8080); 
+app.use(express.static(__dirname));
+
+app.all('/ajax', function(req, res){
+  console.log('request recieved'); 
+  res.send('done');
+});
+
+app.listen(8080);
