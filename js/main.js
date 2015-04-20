f2533eff15658859b54c157f7ff55143a1731448
artsoroka <githubrepository@yandex.ru>
2015-04-20 15:53:50 -0400

    created Confirmator object

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
 (function(window){
-window.onbeforeunload = function (event) {
-  http = new XMLHttpRequest();
-  http.open('POST', '/ajax', true); 
-  http.send('hey=how&pow=woowza'); 
-  
-  var message = 'Sure you want to leave?';
-  if (typeof event == 'undefined') {
-    event = window.event;
-  }
-  if (event) {
-    event.returnValue = message;
-  }
-  return message;
-}
+
+  Confirmator = function(config){
+    return {
+      run: function(){
+        window.onbeforeunload = function (event) {
+    
+          window.onbeforeunload = null;
+    
+          http = new XMLHttpRequest();
+          http.open('POST', '/ajax', true);
+          http.send('hey=how&somithing=else');
+    
+          var message = 'Sure you want to leave?';
+          if (typeof event == 'undefined') {
+            event = window.event;
+          }
+          if (event) {
+            event.returnValue = message;
+          }
+            return message;
+        };
+      }
+    };
+  };
+
+  c = new Confirmator(); 
+  c.run(); 
+
 })(window); 
